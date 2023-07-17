8134356a3a93f03c1b5ba359615a0f29d8ecde71
Arkadiusz Młynarczyk <arkadiusz.mlyn@icloud.com>
2023-07-18 00:05:23 +0200

    Animating bindings

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -11,27 +11,24 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     var body: some View {
         
-        Button("Tap Me"){
-//            animationAmount += 1
-        }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .overlay(
-        Circle()
-            .stroke(.red)
-                    .scaleEffect(animationAmount)
-                    .opacity(2 - animationAmount)
-                    .animation(
+        VStack {
+                    Stepper("Scale amount", value: $animationAmount.animation(
                         .easeInOut(duration: 1)
-                            .repeatForever(autoreverses: false),
-                        value: animationAmount
-                    )
-            )
-            .onAppear {
-                animationAmount = 2
-            }
+                        .repeatCount(3, autoreverses: true)),
+                            in: 1...10)
+//swiftui "fills" the space between 1 and 2 with animation, the animationAmount increase linear 1 2 3 etc.
+                    Spacer()
+
+                    Button("Tap Me") {
+                        animationAmount += 1
+                    }
+                    .padding(40)
+                    .background(.red)
+                    .foregroundColor(.white)
+                    .clipShape(Circle())
+                    .scaleEffect(animationAmount)
+//                    .animation(.linear(duration: 1), value: animationAmount)
+                }
         
         
     }
