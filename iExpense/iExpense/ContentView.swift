9d47e89af1b0126568ad1bbaee746805dcbd4119
Arkadiusz Młynarczyk <arkadiusz.mlyn@icloud.com>
2023-08-11 00:23:41 +0200

    Deleting items using onDelete()

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -24,10 +24,45 @@ struct SecondView: View {
     }
 }
 
+struct ThirdView: View {
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+    var body: some View {
+        NavigationStack{
+            VStack{
+                List {
+                    ForEach(numbers, id: \.self){
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                    
+                }
+                
+                Button ("Add Number"){
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+            }
+            .navigationTitle("onDelete()")
+            .toolbar{
+                EditButton()
+            }
+        }
+        
+    }
+    
+    func removeRows(at offsets: IndexSet){
+        numbers.remove(atOffsets: offsets)
+    }
+}
+
 struct ContentView: View {
     @StateObject var user = User()
     
     @State private var showingSheet = false
+    
+    @State private var showingSheet2 = false
+    
     var body: some View {
         VStack {
             Text("Your name is \(user.firstname) \(user.lastname)")
@@ -36,15 +71,24 @@ struct ContentView: View {
             TextField("First name", text: $user.firstname)
             TextField("Last name", text: $user.lastname)
             
-            Button ("Show sheet"){
+            Button ("Show sheet1"){
                 showingSheet.toggle()
             }
             .sheet(isPresented: $showingSheet) {
                 SecondView(name: user.firstname)
             }
+            
+            Button ("Show sheet2"){
+                showingSheet2.toggle()
+            }
+            .sheet(isPresented: $showingSheet2) {
+                ThirdView()
+            }
         }
         .padding()
     }
+    
+    
 }
 
 #Preview {
