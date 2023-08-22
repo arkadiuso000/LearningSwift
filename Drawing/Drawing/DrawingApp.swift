c8499e6faa33eadf4d0548463f12560e39334390
Arkadiusz Młynarczyk <arkadiusz.mlyn@icloud.com>
2023-08-22 20:40:11 +0200

    Creating custom paths with SwiftUI

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Arkadiusz Młynarczyk on 22/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
