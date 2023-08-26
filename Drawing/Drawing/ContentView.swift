0ca3b6a3b90bda5a01979b354466be971095305a
Arkadiusz Młynarczyk <arkadiusz.mlyn@icloud.com>
2023-08-26 19:04:12 +0200

    Special effects in SwiftUI: blurs, blending, and more

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,49 +7,41 @@
 
 import SwiftUI
 
-struct Flower: Shape {
-    var petalOffset = -20.0
-    var petalWidth = 100.0
-    func path (in rect: CGRect) -> Path {
-        var path = Path()
-        
-        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
-            let rotation = CGAffineTransform(rotationAngle: number)
-            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
-            
-            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
-            
-            
-            let rotatedPetal = originalPetal.applying(position)
-            
-            path.addPath(rotatedPetal)
-        }
-        return path
-    }
-}
+
 
 struct ContentView: View {
-    @State private var petalOffset = -20.0
-    @State private var petalWidth = 100.0
+    @State private var amount = 0.0
     
     var body: some View {
         VStack {
-            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-                .fill(.red, style: FillStyle(eoFill: true))
-            
-            Text("Offset")
-            Slider(value: $petalOffset, in: -40...40)
-                .padding([.horizontal, .bottom])
-            
-            Text("Width")
-            Slider(value: $petalWidth, in: 1...100)
-                .padding(.horizontal)
+            ZStack {
+                Circle()
+                    .fill(Color(red: 1, green: 0, blue: 0))
+                    .frame(width: 200 * amount)
+                    .offset(x: -50, y: -80)
+                    .blendMode(.screen)
                 
+                Circle()
+                    .fill(Color(red: 0, green: 1, blue: 0))
+                    .frame(width: 200 * amount)
+                    .offset(x: 50, y: -80)
+                    .blendMode(.screen)
                 
-            
+                Circle()
+                    .fill(Color(red: 0, green: 0, blue: 1))
+                    .frame(width: 200 * amount)
+                    .blendMode(.screen)
+            }
+            .frame(width: 300, height: 300)
+            Slider(value: $amount)
+                .padding()
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.black)
+        .ignoresSafeArea()
         
     }
+    
 }
 
 #Preview {
